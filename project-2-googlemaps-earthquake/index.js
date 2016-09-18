28a61793c40900925143fc0a5aec79a42fd51da0
dexterleow <gdleow@gmail.com>
2016-09-19 01:38:20 +0800

    Able to signup,login and logout

--- project-2-googlemaps-earthquake/index.js
+++ project-2-googlemaps-earthquake/index.js
@@ -1,30 +1,60 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var passport = require('./config/ppConfig');
+var session = require('express-session');
+var flash = require('connect-flash');
 var morgan = require('morgan');
+var NODE_ENV = require("node-env")
 var db = require('./models');
+var isLoggedIn = require('./middleware/isLoggedIn');
 var app = express();
 var session = require('express-session');
 
 app.set('view engine', 'ejs');
-app.use(require('morgan')('dev'));
-app.use(ejsLayouts);
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(__dirname + '/public/'));
 
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public/'));
 app.use(session({
-  secret: 'Super secrettttt',
+  secret: process.env.SESSION_SECRET || 'donttellanybody',
   resave: false,
   saveUninitialized: true
 }));
+
+/*
+ * Include the flash module by calling it within app.use().
+ * IMPORTANT: This MUST go after the session module
+ */
+app.use(flash());
+// initialize the passport configuration and session as middleware
+app.use(passport.initialize()); //position  and orders are very important. need to initialize before session.
+app.use(passport.session());
+app.use(function(req, res, next) {
+  // before every route, attach the flash messages and current user to res.locals
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.use('/project-2-googlemaps', require('./controllers/project-2-googlemaps'));
+app.get('/profile', function(req, res) {
+  res.render('profile');
+});
+
+app.get('/profile', isLoggedIn, function(req, res) {
+  res.render('profile');
+});
+
+app.use('/auth', require('./controllers/auth'));
 
 console.log("You're listening to the smooth smooth sounds of http://localhost:3000");
+
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
